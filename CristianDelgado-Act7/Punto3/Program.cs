5a9bad75d7ae9f7fbcba8021e8fa860339f72cdd
Cristian Delgado <c6553708@gmail.com>
2026-04-23 08:44:32 -0300

    Add files via upload

---
+++ CristianDelgado-Act7/Punto3/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Punto3
+            //Pedir al usuario que ingrese las puntuaciones obtenidas en 6 partidas seguidas deun videojuego.
+            //El programa debe:
+            // Mostrar la puntuación más alta y la más baja.
+            // Calcular el promedio de puntuación.
+            // Contar cuántas veces superó los 500 puntos.
+
+            int[] puntos = new int[6];
+            int suma = 0;
+            int mayor = int.MinValue;
+            int menor = int.MaxValue;
+            int mayores500 = 0;
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.Write("Ingrese puntuación: ");
+                puntos[i] = int.Parse(Console.ReadLine());
+
+                suma += puntos[i];
+
+                if (puntos[i] > mayor)
+                    mayor = puntos[i];
+
+                if (puntos[i] < menor)
+                    menor = puntos[i];
+
+                if (puntos[i] > 500)
+                    mayores500++;
+            }
+
+            double promedio = (double)suma / 6;
+
+            Console.WriteLine("Mayor puntuación: " + mayor);
+            Console.WriteLine("Menor puntuación: " + menor);
+            Console.WriteLine("Promedio: " + promedio);
+            Console.WriteLine("Veces que superó 500: " + mayores500);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CristianDelgado-Act7/Punto4/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Punto4:
+            // Cargar un vector de 10 elementos y verificar posteriormente si el mismo está
+            // ordenado de menor a mayor y de mayor a menor.
+
+            int[] vector = new int[10];
+            bool ascendente = true;
+            bool descendente = true;
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("Ingrese valor: ");
+                vector[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < 9; i++)
+            {
+                if (vector[i] < vector[i + 1])
+                    descendente = false;
+
+                if (vector[i] > vector[i + 1])
+                    ascendente = false;
+            }
+
+            if (ascendente)
+                Console.WriteLine("Está ordenado de menor a mayor");
+
+            if (descendente)
+                Console.WriteLine("Está ordenado de mayor a menor");
+
+            if (!ascendente && !descendente)
+                Console.WriteLine("No está ordenado");
+                Console.ReadKey();
+        }
+    }
+}
