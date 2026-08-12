0959d8fe110ccf56c4d140b2ae8da44b5dfaf6a8
Cristian Delgado <c6553708@gmail.com>
2026-08-12 12:57:06 -0300

    Add files via upload

---
+++ CristianDelgado-Act23/Punto3/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto3
+{
+    // Punto3:
+    // Un centro de conservación marina registra de forma dinámica las especies de
+    // mamíferos marinos divisadas en la costa para su posterior análisis estadístico.
+    //  Crear la clase MonitoreoCostero que contenga como atributo privado
+    // una lista de cadenas de texto List<string> especiesDetectadas.
+    //  Métodos en MonitoreoCostero:
+    // 1. CargarAvistamientos() : Solicitar por teclado nombres de
+    // especies marinas avistadas(ej: &quot; Ballena Franca&quot;, &quot;Lobo Marino&quot;,
+    // &quot;Delfín&quot;) y agregarlos a la lista utilizando.Add(). La carga finaliza
+    // cuando el usuario ingresa la palabra & quot; FIN&quot;.
+    // 2. MostrarReporteOrdenado() : Imprimir la lista de avistamientos
+    // organizada alfabéticamente de la A a la Z utilizando el método .Sort().
+    // 3. MostrarReporteInvertido() : Imprimir la lista organizada de la Z a la A combinando.Sort() con el método.Reverse().
+    // 4. BuscarEspecie() : Pedir al operador que ingrese el nombre de un
+    // animal y, utilizando los métodos de búsqueda de listas, informar si la especie fue divisada en la costa durante el día.
+
+    class MonitoreoCostero
+    {
+        List<string> especiesDetectadas = new List<string>();
+
+        public void cargarAvistamientos()
+        {
+            bool bucle = true;
+            do
+            {
+                Console.Write("Ingrese la especie marina avistada o *FIN* para dejar de ingresar datos: ");
+                string texto = Console.ReadLine();
+                if (texto != "FIN")
+                {
+                    especiesDetectadas.Add(texto);
+                }
+                else
+                {
+                    bucle = false;
+                }
+
+            } while (bucle == true);
+            Console.WriteLine();
+        }
+
+        public void mostrarReporteOrdenado()
+        {
+            especiesDetectadas.Sort();
+            Console.WriteLine();
+            Console.WriteLine("Reporte ordenado:");
+
+            for (int i = 0; i < especiesDetectadas.Count; i++)
+            {
+                Console.WriteLine($"- {especiesDetectadas[i]}");
+            }
+        }
+
+        public void mostrarReporteInvertido()
+        {
+            especiesDetectadas.Sort();
+            especiesDetectadas.Reverse();
+            Console.WriteLine();
+            Console.WriteLine("Reporte invertido:");
+
+            for (int i = 0; i < especiesDetectadas.Count; i++)
+            {
+                Console.WriteLine($"- {especiesDetectadas[i]}");
+            }
+        }
+
+        public void buscarEspecie()
+        {
+            {
+                string especie;
+
+                Console.WriteLine();
+                Console.Write("Ingrese el nombre de la especie a buscar: ");
+                especie = Console.ReadLine();
+
+                if (especiesDetectadas.Contains(especie))
+                {
+                    Console.WriteLine("La especie fue divisada en la costa.");
+                }
+                else
+                {
+                    Console.WriteLine("La especie no fue divisada en la costa.");
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            MonitoreoCostero mc1 = new MonitoreoCostero();
+            mc1.cargarAvistamientos();
+            mc1.mostrarReporteOrdenado();
+            mc1.mostrarReporteInvertido();
+            mc1.buscarEspecie();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CristianDelgado-Act23/Punto6/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto6
+{
+    // Punto6:
+    // Una biblioteca pública gestiona de forma digital su catálogo de obras literarias
+    // para organizar la consulta de ejemplares por parte de los lectores.
+    //  Crear la clase Libro que contenga como atributos privados: titulo
+    // (string) y anioPublicacion(int). Definir sus propiedades de solo lectura y un constructor que reciba tit y anio.
+    //  Crear la clase BibliotecaCentral que administre una lista de objetos List.
+    //  Métodos en BibliotecaCentral:
+    // 1. CargarCatalogo(): Solicitar por teclado títulos y años de
+    // publicación para agregar libros a la lista mediante.Add(). La carga
+    // finaliza cuando el usuario ingresa la palabra &quot; FIN&quot; como título.
+    // 2. ListarCatalogo(): Mostrar en pantalla todos los libros del catálogo
+    // junto a la cantidad total de obras registradas mediante la propiedad .Count.
+    // 3. FiltrarPorAnio(): Solicitar al usuario un año determinado e imprimir en consola únicamente aquellos libros cuyo año de
+    // publicación sea menor a dicho valor.
+    // 4. RemoverLibro(): Pedir al usuario el título de un libro y, utilizando
+    // los métodos de búsqueda y remoción de listas, eliminarlo de la colección si se encuentra presente.
+
+    class Libro
+    {
+        private string titulo;
+        private int anioPublicacion;
+
+        public string Titulo
+        {
+            get { return titulo; }
+        }
+
+        public int AnioPublicacion
+        {
+            get { return anioPublicacion; }
+        }
+
+        public Libro(string tit, int anio)
+        {
+            this.titulo = tit;
+            this.anioPublicacion = anio;
+        }
+    }
+    class BibliotecaCentral
+    {
+        List<Libro> libros = new List<Libro>();
+
+        public void CargarCatalogo()
+        {
+            string titulo;
+            do
+            {
+                Console.Write("Ingrese el titulo del libro (FIN para terminar): ");
+                titulo = Console.ReadLine();
+                if (titulo.ToUpper() != "FIN")
+                {
+                    Console.Write("Ingrese el año de publicacion: ");
+                    int anio = int.Parse(Console.ReadLine());
+                    Libro nuevoLibro = new Libro(titulo, anio);
+                    libros.Add(nuevoLibro);
+                    Console.WriteLine();
+                }
+
+            } while (titulo.ToUpper() != "FIN");
+
+            Console.WriteLine();
+        }
+
+        public void ListarCatalogo()
+        {
+            Console.WriteLine("CATALOGO DE LIBROS");
+            foreach (Libro libro in libros)
+            {
+                Console.WriteLine($"- {libro.Titulo} - Año: {libro.AnioPublicacion}");
+            }
+
+            Console.WriteLine($"Cantidad total de obras registradas: {libros.Count}");
+            Console.WriteLine();
+        }
+
+        public void FiltrarPorAnio()
+        {
+            Console.Write("Ingrese un año para realizar el filtro: ");
+            int anio = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine($"*** LIBROS PUBLICADOS ANTES DEL AÑO {anio} ***");
+
+            foreach (Libro libro in libros)
+            {
+                if (libro.AnioPublicacion < anio)
+                {
+                    Console.WriteLine($"- {libro.Titulo} - Año: {libro.AnioPublicacion}");
+                }
+            }
+
+            Console.WriteLine();
+        }
+
+        public void RemoverLibro()
+        {
+            Console.Write("Ingrese el titulo del libro a remover: ");
+            string titulo = Console.ReadLine();
+
+            bool existe = false;
+
+            for (int i = 0; i < libros.Count; i++)
+            {
+                if (libros[i].Titulo == titulo)
+                {
+                    libros.RemoveAt(i);
+                    existe = true;
+                    Console.WriteLine("El libro fue eliminado correctamente.");
+                    break;
+                }
+            }
+
+            if (existe == false)
+            {
+                Console.WriteLine("El libro no se encuentra en el catalogo.");
+            }
+
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            BibliotecaCentral bc = new BibliotecaCentral();
+            bc.CargarCatalogo();
+            bc.ListarCatalogo();
+            bc.FiltrarPorAnio();
+            bc.RemoverLibro();
+            bc.ListarCatalogo();
+
+            Console.ReadKey();
+        }
+    }
+}
