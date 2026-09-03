da0b70eb325e3ecb949654c160db29cdf7134e0d
Cristian Delgado <c6553708@gmail.com>
2026-09-03 12:37:03 -0300

    Add files via upload

---
+++ CristianDelgado-Act28/Punto1/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Resources.ResXFileRef;
+
+namespace Punto1
+{
+    // Punto1: 1. Calculadora de Promedio de Notas
+    // ● Consigna: Crear un formulario con tres TextBox para ingresar notas y un Button
+    // &quot;Calcular&quot;. Convertir los valores con int.Parse() o double.Parse() y mostrar en una
+    // Label el promedio.Si la nota es mayor o igual a 6, cambiar el color del texto de la
+    // etiqueta a verde; de lo contrario, a rojo.
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double nota1 = double.Parse(textBox1.Text);
+            double nota2 = double.Parse(textBox2.Text);
+            double nota3 = double.Parse(textBox3.Text);
+
+            double promedio = (nota1 + nota2 + nota3) / 3;
+
+            label4.Text = "Promedio: " + promedio;
+
+            if (promedio >= 6)
+            {
+                label4.ForeColor = Color.Green;
+            }
+            else
+            {
+                label4.ForeColor = Color.Red;
+            }
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ CristianDelgado-Act28/Punto2/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Punto2
+{
+    // Punto2: Conversor de Temperatura
+    // ● Consigna: Disponer un TextBox para el ingreso numérico y dos RadioButton:
+    // &quot;Celsius a Fahrenheit&quot; y &quot;Fahrenheit a Celsius&quot;. Al presionar un Button, realizar la
+    // fórmula correspondiente y mostrar el resultado en un Label.
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double temperatura = double.Parse(textBox1.Text);
+            double resultado = 0;
+
+            if (radioButton1.Checked)
+            {
+                resultado = (temperatura * 9 / 5) + 32;
+            }
+
+            if (radioButton2.Checked)
+            {
+                resultado = (temperatura - 32) * 5 / 9;
+            }
+
+            label1.Text = "Resultado: " + resultado;
+        }
+    }
+}
