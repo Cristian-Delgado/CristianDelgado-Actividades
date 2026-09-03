7d310688756e61c727619ac35ab95b6d7abf1f08
Cristian Delgado <c6553708@gmail.com>
2026-09-03 10:32:26 -0300

    Add files via upload

---
+++ CristianDelgado-Act25/Punto3/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Punto3
+{
+    // Punto3:
+    // Crear una aplicación que muestre en 6 objetos de la clase Label con
+    // algunos nombres de controles visuales contenidos en la pestaña de
+    // "controles comunes" del cuadro de herramientas
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
