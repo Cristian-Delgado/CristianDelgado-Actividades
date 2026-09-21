81482942bbda4427df44d61e7a1e8fcf59dcce9e
Cristian Delgado <c6553708@gmail.com>
2026-09-21 16:35:36 -0300

    Add files via upload

---
+++ CristianDelgado-Act28/Punto3/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Punto3
+{
+    // Punto3: Gestor de Tareas Simplificado
+    // ● Consigna: Armar una interfaz con un TextBox, un Button &quot;Agregar&quot;, un Button
+    // &quot;Eliminar&quot; y un ListBox.Permitir añadir el texto tipeado a la lista y borrar el elemento
+    // que el usuario seleccione.
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                listBox1.Items.Add(textBox1.Text);
+                textBox1.Text = "";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex >= 0)
+            {
+                listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+            }
+        }
+    }
+}
---
+++ CristianDelgado-Act28/Punto4/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Punto4
+{
+    // Punto4: Temporizador de Clics
+    // ● Consigna: Crear un mini-juego donde un Button cuente cuántos clics realiza el
+    // usuario en 10 segundos.Al finalizar el tiempo mediante el Timer, deshabilitar el
+    // botón(Enabled = false) y mostrar el puntaje acumulado en un MessageBox.Show.
+
+    public partial class Form1 : Form
+    {
+        int clics = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            timer1.Interval = 10000;
+            button1.Click += button1_Click;
+            timer1.Tick += timer1_Tick;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            clics++;
+            if (clics == 1)
+            {
+                timer1.Start();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            button1.Enabled = false;
+            MessageBox.Show("Hiciste " + clics + " clics en 10 segundos.");
+        }
+    }
+}
